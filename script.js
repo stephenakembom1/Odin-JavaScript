d0db6d43f9bb8505b782f57def2bac6acc49da76
Akembom Stephen <akembomcedric@gmail.com>
2025-05-22 14:04:17 +0100

    modified:   script.js with variables to declare scores

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * choices.length);
