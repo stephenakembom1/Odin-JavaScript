fe8c2f367eabc1be6213909b0918a216b33ef7fc
Akembom Stephen <akembomcedric@gmail.com>
2025-05-22 14:20:50 +0100

    modified:   script.js with a function to playerround

--- script.js
+++ script.js
@@ -19,3 +19,19 @@ function getHumanChoice() {
     return choice;
 }
 console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "It's a tie!";
+    } else if (
+        (humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
+        humanScore++;
+        return `You win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        computerScore++;
+        return `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
