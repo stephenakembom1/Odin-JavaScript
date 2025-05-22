143b9363fc91199d1431a05160190b12c1b7dd6d
Akembom Stephen <akembomcedric@gmail.com>
2025-05-22 13:28:13 +0100

    modified index.html deleted javascript.js added new file script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+
+}
+console.log(getComputerChoice());
